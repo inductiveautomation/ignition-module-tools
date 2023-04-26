66c6a3e50a91a78b83a1f66d4f4fa96663be6e76
Perry Jones <perryjones@gmail.com>
2023-04-26 08:18:22 -0700

    module project using gradle 7.6 and required fixes
    (cherry picked from commit b61da785818ad5de471d139c87cfe7225098184f)

--- gradle-module-plugin/src/main/kotlin/io/ia/sdk/gradle/modl/task/Deploy.kt
+++ gradle-module-plugin/src/main/kotlin/io/ia/sdk/gradle/modl/task/Deploy.kt
@@ -5,6 +5,7 @@ import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
@@ -39,7 +40,7 @@ open class Deploy @javax.inject.Inject constructor(objects: ObjectFactory) : Def
         hostGateway.set(url)
     }
 
-    @get:Input
+    @get:InputFile
     val module: RegularFileProperty = objects.fileProperty()
 
     @Option(option = "module", description = "Signed module file to be deployed to the running dev gateway")
