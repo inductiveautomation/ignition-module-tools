1a3814a2866c82c0197567036fe232ae6345c04e
Brian Ray <brianeray@users.noreply.github.com>
2024-03-22 09:01:51 -0700

    IGN-7871 Final release version 0.2.0

--- gradle-module-plugin/build.gradle.kts
+++ gradle-module-plugin/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 }
 
 group = "io.ia.sdk"
-version = "0.2.0-SNAPSHOT" // FIXME non-snapshot upon release
+version = "0.2.0"
 
 configurations {
     val functionalTestImplementation by registering {
